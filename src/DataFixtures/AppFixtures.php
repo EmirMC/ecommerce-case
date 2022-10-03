7667a87cc9c118aeaeac8a88ba1275bdc478a5b1
Emir Mert ÇAMLI <51798175+EmirMC@users.noreply.github.com>
2022-10-03 06:10:11 +0300

    Demo user fixtures created

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 3; $i++) {
+            $user = new User();
+            $user->setEmail('demo' . $i . '@demo.com');
+            $password = $this->hasher->hashPassword($user, 'demo123');
+            $user->setPassword($password);
+            $user->setRoles(["ROLE_CUSTOMER"]);
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
